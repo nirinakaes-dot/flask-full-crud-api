6c4aa3c9214ae296265339204a0434a5c7b39582
Nina Adora <nirinakaes@gmail.com>
2026-06-29 11:27:27 +0300

    Add typing information for pip and create venv configuration file
    - Introduced a new `py.typed` file in the pip package to indicate that typing information is provided for convenience, with a warning against using internal APIs.
    - Created a `pyvenv.cfg` file for the virtual environment, specifying the home directory, system site packages inclusion, Python version, executable path, and command used to create the virtual environment.

--- app.py
+++ app.py
@@ -22,22 +22,34 @@ events = [
 @app.route("/events", methods=["POST"])
 def create_event():
     # TODO: Task 2 - Design and Develop the Code
-
+    data = request.get_json()
+    
     # TODO: Task 3 - Implement the Loop and Process Each Element
+    new_ID = len(events) + 1
+    new_event = Event(id=new_ID, title=data["title"])
 
+
+    events.append(new_event)
     # TODO: Task 4 - Return and Handle Results
-    pass
+    return jsonify(new_event.to_dict()),201
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
     # TODO: Task 2 - Design and Develop the Code
+    data = request.get_json()
 
     # TODO: Task 3 - Implement the Loop and Process Each Element
+    event = next((e for e in events if e.id == event_id), None)
 
+    if not event:
+        return ("Event not found", 404)
+    if "title" in data:
+        event.title = data["title"]
     # TODO: Task 4 - Return and Handle Results
-    pass
+    return jsonify(event.to_dict())
+    
 
 # TODO: Task 1 - Define the Problem
 # Remove an event from the list
@@ -46,9 +58,14 @@ def delete_event(event_id):
     # TODO: Task 2 - Design and Develop the Code
 
     # TODO: Task 3 - Implement the Loop and Process Each Element
+    deleted = next((e for e in events if e.id == event_id), None)
+    if not deleted:
+        return ("Event not found", 404)
 
     # TODO: Task 4 - Return and Handle Results
-    pass
+
+    events = [e for e in events if e.id != id]
+    return ("Event deleted", 204)
 
 if __name__ == "__main__":
     app.run(debug=True)
